a156551ac56764ba6fcc8e5c6756caf775904726
Gustavsson <basse989@hotmail.com>
2022-03-01 15:41:06 +0100

    update

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -27,13 +27,16 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
      };
 
     return <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
